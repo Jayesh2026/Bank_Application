ef626c91f037da6997e4526b143ad2c9ae9b54a1
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-05-27 12:16:32 +0530

    added validation dependency

--- src/main/java/com/example/model/Account.java
+++ src/main/java/com/example/model/Account.java
@@ -7,13 +7,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -25,10 +26,21 @@ public class Account {
     @Column(name = "Account UUID")
     private UUID accountId;
 
+    @NotNull(message = "Account number can not nbe null.")
+    @Column(name = "account_number", nullable = false)
     private Integer accountNumber;
-    private String accountType;
+
+    @Size(min = 3, max = 10 , message = "Account type can not be null")
+    @Column(name = "account_type", nullable = false, columnDefinition = "VARCHAR(10) DEFAULT 'saving'")
+    private String accountType = "saving";
+
+    @Pattern(regexp = "^[A-Z]{3}$", message = "Please enter 3 character of Currency code")
+    @Column(name = "iso4217_currency_code")
     private String iso4217Currency;
-    private Integer balance;
+
+    @NotNull(message = "Balance should not be negative")
+    @Column(columnDefinition = "INTEGER DEFAULT 0", nullable = false)
+    private Integer balance = 0;
 
     private boolean deleted = Boolean.FALSE;
 
